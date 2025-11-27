5d952b5c07529e1f8b4a04625dc3b79accef7371
yusamercan058 <yusamercan058@gmail.com>
2025-11-27 22:34:13 +0300

    Fix build errors final

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
-// /// <reference types="vite/client" />
+// Fixed "Cannot find type definition file for 'vite/client'" by removing the reference.
+// Fixed "Cannot redeclare block-scoped variable 'process'" by using namespace augmentation.
 
-declare var process: {
-  env: {
+declare namespace NodeJS {
+  interface ProcessEnv {
     API_KEY?: string;
     [key: string]: any;
   }
